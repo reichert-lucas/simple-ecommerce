c7754d6f6a659a6a00e6df6ca9b2dd7fb7e4b959
Lucas Coelho Reichert <50890378+lucas3d4783@users.noreply.github.com>
2022-04-14 04:03:52 -0300

    Refatoração da rota welcome

--- routes/web.php
+++ routes/web.php
@@ -19,10 +19,8 @@ Route::get('/', function () {
     return Inertia::render('Welcome', [
         'canLogin' => Route::has('login'),
         'canRegister' => Route::has('register'),
-        'laravelVersion' => Application::VERSION,
-        'phpVersion' => PHP_VERSION,
     ]);
-});
+})->name('welcome');
 
 Route::middleware([
     'auth:sanctum',
