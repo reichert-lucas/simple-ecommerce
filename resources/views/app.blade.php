7f130c113eeb723357db8921f4668ebdf645a3b9
Lucas Coelho Reichert <50890378+lucas3d4783@users.noreply.github.com>
2022-04-14 04:04:12 -0300

    Adição da CDN iconscout

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -13,6 +13,7 @@
 
         <!-- Styles -->
         <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+        <link rel="stylesheet" href="https://unicons.iconscout.com/release/v4.0.0/css/line.css">
 
         <!-- Scripts -->
         @routes
