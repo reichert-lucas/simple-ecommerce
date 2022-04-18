2a2797190113ee4581a35547d1e6813ef9a7342f
Lucas Coelho Reichert <50890378+lucas3d4783@users.noreply.github.com>
2022-04-18 01:19:20 -0300

    Separando a responsabilidade do NavBar

---
+++ resources/js/Components/NavBar/NavBar.vue
@@ -0,0 +1,99 @@
+<script setup>
+import { ref } from 'vue'
+import { Inertia } from '@inertiajs/inertia'
+import { Head, Link } from '@inertiajs/inertia-vue3'
+import JetApplicationMark from '@/Jetstream/ApplicationMark.vue'
+import JetBanner from '@/Jetstream/Banner.vue'
+import JetDropdown from '@/Jetstream/Dropdown.vue'
+import JetDropdownLink from '@/Jetstream/DropdownLink.vue'
+import JetNavLink from '@/Jetstream/NavLink.vue'
+import JetResponsiveNavLink from '@/Jetstream/ResponsiveNavLink.vue'
+import NavShoppingCart from '@/Components/NavShoppingCart.vue'
+import ProductList from '@/Components/Products/List.vue'
+import Header from '@/Components/Header/Header.vue'
+import Spin from '@/Components/Icons/Spin.vue'
+
+const showingNavigationDropdown = ref(false);
+
+</script>
+
+<template>
+    <nav class="bg-white border-b border-gray-100">
+        <!-- Primary Navigation Menu -->
+        <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+            <div class="flex justify-between h-16">
+                <div class="flex">
+                    <!-- Logo -->
+                    <div class="shrink-0 flex items-center">
+                        <Link :href="route('welcome')">
+                            <JetApplicationMark class="block h-9 w-auto" />
+                        </Link>
+                    </div>
+
+                    <!-- Navigation Links -->
+                    <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
+                        <JetNavLink :href="route('welcome')" :active="route().current('welcome')">
+                            Categorias
+                        </JetNavLink>
+                    </div>
+                    <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
+                        <JetNavLink :href="route('welcome')" :active="route().current('welcome')">
+                            Produtos
+                        </JetNavLink>
+                    </div>
+                </div>
+
+                <div class="search-container">
+
+                </div>
+
+                <NavShoppingCart />
+
+                <!-- Hamburger -->
+                <div class="-mr-2 flex items-center sm:hidden">
+                    <button class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-500 transition" @click="showingNavigationDropdown = !showingNavigationDropdown">
+                        <svg
+                            class="h-6 w-6"
+                            stroke="currentColor"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                        >
+                            <path
+                                :class="{'hidden': showingNavigationDropdown, 'inline-flex': !showingNavigationDropdown }"
+                                stroke-linecap="round"
+                                stroke-linejoin="round"
+                                stroke-width="2"
+                                d="M4 6h16M4 12h16M4 18h16"
+                            />
+                            <path
+                                :class="{'hidden': ! showingNavigationDropdown, 'inline-flex': showingNavigationDropdown }"
+                                stroke-linecap="round"
+                                stroke-linejoin="round"
+                                stroke-width="2"
+                                d="M6 18L18 6M6 6l12 12"
+                            />
+                        </svg>
+                    </button>
+                </div>
+            </div>
+        </div>
+
+        <!-- Responsive Navigation Menu -->
+        <div :class="{'block': showingNavigationDropdown, 'hidden': !showingNavigationDropdown}" class="sm:hidden">
+            <div class="pt-2 pb-3 space-y-1">
+                <JetResponsiveNavLink :href="route('dashboard')" :active="route().current('dashboard')">
+                    Dashboard
+                </JetResponsiveNavLink>
+            </div>
+
+            <!-- Responsive Settings Options -->
+            <div class="pt-4 pb-1 border-t border-gray-200">
+                <div class="mt-3 space-y-1">
+                    <JetResponsiveNavLink :href="route('welcome')">
+                        teste de item
+                    </JetResponsiveNavLink>
+                </div>
+            </div>
+        </div>
+    </nav>
+</template>
--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -1,118 +1,25 @@
 <script setup>
-import { ref } from 'vue'
-import { Inertia } from '@inertiajs/inertia'
-import { Head, Link } from '@inertiajs/inertia-vue3'
-import JetApplicationMark from '@/Jetstream/ApplicationMark.vue'
-import JetBanner from '@/Jetstream/Banner.vue'
-import JetDropdown from '@/Jetstream/Dropdown.vue'
-import JetDropdownLink from '@/Jetstream/DropdownLink.vue'
-import JetNavLink from '@/Jetstream/NavLink.vue'
-import JetResponsiveNavLink from '@/Jetstream/ResponsiveNavLink.vue'
-import NavShoppingCart from '@/Components/NavShoppingCart.vue'
-import ProductList from '@/Components/Products/List.vue'
+import NavBar from '@/Components/NavBar/NavBar.vue'
 import Header from '@/Components/Header/Header.vue'
 import Spin from '@/Components/Icons/Spin.vue'
+import ProductList from '@/Components/Products/List.vue'
 
-
-const showingNavigationDropdown = ref(false);
-
-defineProps({
-    canLogin: Boolean,
-    canRegister: Boolean,
-});
 </script>
 
 <template>
     <div class="min-h-screen bg-gray-100">
-        <nav class="bg-white border-b border-gray-100">
-                <!-- Primary Navigation Menu -->
-                <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-                    <div class="flex justify-between h-16">
-                        <div class="flex">
-                            <!-- Logo -->
-                            <div class="shrink-0 flex items-center">
-                                <Link :href="route('welcome')">
-                                    <JetApplicationMark class="block h-9 w-auto" />
-                                </Link>
-                            </div>
+        <NavBar />
 
-                            <!-- Navigation Links -->
-                            <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
-                                <JetNavLink :href="route('welcome')" :active="route().current('welcome')">
-                                    Categorias
-                                </JetNavLink>
-                            </div>
-                            <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
-                                <JetNavLink :href="route('welcome')" :active="route().current('welcome')">
-                                    Produtos
-                                </JetNavLink>
-                            </div>
-                        </div>
+        <Header />
 
-                        <div class="search-container">
+        <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+            <ProductList :products="products" v-if="!loadingProducts"/>
 
-                        </div>
-
-                        <NavShoppingCart />
-
-                        <!-- Hamburger -->
-                        <div class="-mr-2 flex items-center sm:hidden">
-                            <button class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-500 transition" @click="showingNavigationDropdown = !showingNavigationDropdown">
-                                <svg
-                                    class="h-6 w-6"
-                                    stroke="currentColor"
-                                    fill="none"
-                                    viewBox="0 0 24 24"
-                                >
-                                    <path
-                                        :class="{'hidden': showingNavigationDropdown, 'inline-flex': !showingNavigationDropdown }"
-                                        stroke-linecap="round"
-                                        stroke-linejoin="round"
-                                        stroke-width="2"
-                                        d="M4 6h16M4 12h16M4 18h16"
-                                    />
-                                    <path
-                                        :class="{'hidden': ! showingNavigationDropdown, 'inline-flex': showingNavigationDropdown }"
-                                        stroke-linecap="round"
-                                        stroke-linejoin="round"
-                                        stroke-width="2"
-                                        d="M6 18L18 6M6 6l12 12"
-                                    />
-                                </svg>
-                            </button>
-                        </div>
-                    </div>
-                </div>
-
-                <!-- Responsive Navigation Menu -->
-                <div :class="{'block': showingNavigationDropdown, 'hidden': !showingNavigationDropdown}" class="sm:hidden">
-                    <div class="pt-2 pb-3 space-y-1">
-                        <JetResponsiveNavLink :href="route('dashboard')" :active="route().current('dashboard')">
-                            Dashboard
-                        </JetResponsiveNavLink>
-                    </div>
-
-                    <!-- Responsive Settings Options -->
-                    <div class="pt-4 pb-1 border-t border-gray-200">
-                        <div class="mt-3 space-y-1">
-                            <JetResponsiveNavLink :href="route('welcome')">
-                                teste de item
-                            </JetResponsiveNavLink>
-                        </div>
-                    </div>
-                </div>
-            </nav>
-
-            <Header />
-
-            <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-                <ProductList :products="products" v-if="!loadingProducts"/>
-
-                <div class="not__found__container w-full py-6 my-6" v-else>
-                    <Spin class="animate-spin animate-spin"/>
-                    <h4 class="text-center">Carregando seus produtos...</h4>
-                </div>
+            <div class="not__found__container w-full py-6 my-6" v-else>
+                <Spin class="animate-spin animate-spin"/>
+                <h4 class="text-center">Carregando seus produtos...</h4>
             </div>
+        </div>
     </div>
 </template>
 
